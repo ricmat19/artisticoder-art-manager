975c8f8577f31a68f2e1f797084f3843445b8249
Ricardo Del Cueto <ric19mat@gmail.com>
2024-06-02 11:05:54 -0400

    Setup Client

--- API/Program.cs
+++ API/Program.cs
@@ -9,11 +9,11 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
-app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 app.Run();
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit {
+
+  title = "Art Manager";
+  users: any;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/users").subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log("Request complete!")
+    });
+  }
+
+}
